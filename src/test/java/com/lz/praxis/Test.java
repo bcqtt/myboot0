10a255a83ad944326b2a60f0b947294dad85eec1
laizhiwen <laizhiwen@evergrande.com>
2018-11-06 10:25:34 +0800

    test
    test

---
+++ src/test/java/com/lz/praxis/Test.java
@@ -0,0 +1,16 @@
+package com.lz.praxis;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		for(int i=0;i<10000;i++) {
+			if(i%2==0) {
+				System.out.println(0);
+			}else {
+				System.out.println(1);
+			}
+			
+		}
+	}
+
+}
