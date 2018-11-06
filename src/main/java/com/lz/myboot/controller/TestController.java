6f83191ef0322a7b616feb8193cf329c08ec3c9c
laizhiwen <laizhiwen@evergrande.com>
2018-11-06 16:39:04 +0800

    test
    test

--- src/main/java/com/lz/myboot/controller/TestController.java
+++ src/main/java/com/lz/myboot/controller/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-	@RequestMapping("/test1")
-	public String test1() {
+	@RequestMapping("/testApi")
+	public String testApi() {
 		System.out.println("测试访问");
 		
 		return "success";
